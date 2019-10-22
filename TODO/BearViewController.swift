6c55e31e02ea39ac2ca707cecc62ca36851d5c9d
金占军 <519511646@qq.com>
2019-10-22 22:16:24 +0800

    添加在菜单栏滑动控制mainNavView左右滑动

--- TODO/BearViewController.swift
+++ TODO/BearViewController.swift
@@ -50,10 +50,9 @@ class BearViewController: UIViewController {
         /// 添加拖动手势
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
         mainNavController?.view.addGestureRecognizer(panGestureRecognizer)
-
     }
     
-    // 拖动手势相应
+    // 拖动手势响应
     @objc func handlePanGesture(_ recognizer: UIPanGestureRecognizer) {
         
         guard let originX = recognizer.view?.frame.origin.x
@@ -69,30 +68,46 @@ class BearViewController: UIViewController {
                 // 加入侧面菜单方法
                 addMenuViewController()
             }
-            // 如果是正在滑动，则偏移主视图的坐标实现跟随手指位置移动
+        // 如果是正在滑动，则偏移主视图的坐标实现跟随手指位置移动
         case .changed:
-            let positionX = originX + recognizer.translation(in: view).x
-            // 页面滑到最左侧的话就不许继续往左移动
-            recognizer.view?.frame.origin.x = positionX < 0 ? 0 : positionX
-            recognizer.setTranslation(.zero, in: view)
+            // 如果是在mainNav页面中滑动
+            if recognizer.view == mainNavController?.view {
+                let positionX = originX + recognizer.translation(in: view).x
+                // 页面滑到最左侧的话就不许继续往左移动
+                mainNavController?.view?.frame.origin.x = positionX < 0 ? 0 : positionX
+                recognizer.setTranslation(.zero, in: view)
+            }
+            // 如果是在菜单栏中滑动
+            else {
+                let positionX = mainNavController?.view.frame.origin.x
+                mainNavController?.view.frame.origin.x = positionX! + recognizer.translation(in: view).x
+                recognizer.setTranslation(.zero, in: view)
+            }
+
             
         case .ended:
-            // 根据页面滑动是否过半，判断后面是自动展开还是收缩
-            let hasMovedhanHalfway = { () -> Bool in
-                if (recognizer.view?.center.x)! > self.view.bounds.size.width || recognizer.velocity(in: self.view).x > CGFloat(1000) {
-                    return true
+            // 如果是在mainNav界面中滑动
+            if recognizer.view == mainNavController?.view {
+                // 根据页面滑动是否过半，判断后面是自动展开还是收缩
+                let hasMovedhanHalfway = { () -> Bool in
+                    if (recognizer.view?.center.x)! > self.view.bounds.size.width || recognizer.velocity(in: self.view).x > CGFloat(1000) {
+                        return true
+                    }
+                    return false
                 }
-                
-                return false
+                // 自动展开方法
+                animateMainView(shouldExpand: hasMovedhanHalfway())
             }
-            // 自动展开方法
-            animateMainView(shouldExpand: hasMovedhanHalfway())
+            // 如果是在菜单栏中滑动
+            else{
+                animateMainView(shouldExpand: recognizer.velocity(in: view).x > 0)
+            }
         default:
             break
         }
     }
     
-    // 单击手势相应
+    // 单击手势响应
     @objc func handleTapGesture() {
         if currentState == .Expanded {
             animateMainView(shouldExpand: false)
@@ -102,14 +117,13 @@ class BearViewController: UIViewController {
     func addMenuViewController() {
         if menuNavController == nil {
             ///FIXME: 是否是指向同一个实例？？
-            guard let menuNavController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MenuNavController") as? MenuNavController
-                else {return}
+            menuNavController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MenuNavController") as? MenuNavController
             // 插入当前视图并置顶
-            view.insertSubview(menuNavController.view, at: 0)
+            view.insertSubview(menuNavController!.view, at: 0)
             
             // 建立父子关系
-            addChild(menuNavController)
-            menuNavController.didMove(toParent: self)
+            addChild(menuNavController!)
+            menuNavController!.didMove(toParent: self)
         }
     }
     
@@ -120,12 +134,19 @@ class BearViewController: UIViewController {
             // 更新当前状态
             currentState = .Expanded
             // 动画
-            animateMainViewXposition(targetPosition: mainNavController!.view.frame.width - menuNavViewExpandedOffset)
-            /// 添加单击手势
-            tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
-            mainNavController?.view.addGestureRecognizer(tapGestureRecognizer!)
-            // 设置主页面不可交互
-            (mainNavController?.children[0] as? CategoryTableViewController)?.tableView.isScrollEnabled = false
+            //            animateMainViewXposition(targetPosition: mainNavController!.view.frame.width - menuNavViewExpandedOffset)
+            
+            animateMainViewXposition(targetPosition: mainNavController!.view.frame.width - menuNavViewExpandedOffset) { (isComplete) in
+                /// 添加单击手势
+                self.tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleTapGesture))
+                self.mainNavController?.view.addGestureRecognizer(self.tapGestureRecognizer!)
+                // 设置主页面不可滚动
+                (self.mainNavController?.children[0] as? CategoryTableViewController)?.tableView.isScrollEnabled = false
+                
+                /// 添加拖动手势
+                let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.handlePanGesture(_:)))
+                self.menuNavController?.view.addGestureRecognizer(panGestureRecognizer)
+            }
         }
         // 如果用于隐藏
         else {
